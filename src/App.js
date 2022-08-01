e227f4a76dcde68e72a73fccd649836c043c9b48
Abdullo <abdullofayzullayev68@gmail.com>
2022-08-01 07:22:23 +0300

    added detete and edit button

--- src/App.js
+++ src/App.js
@@ -4,47 +4,50 @@ import "./App.scss";
 import ListItem from "./components/List/ListItem";
 
 function App() {
-  const [todo, setTodos] = useState([
-   {
-    name: "Uxlash",
-    isComplate: true,
-   }
-  ]);
+  const [todo, setTodos] = useState(JSON.parse(window.localStorage.getItem("Todos"))||[]);
 
-  let count=0
+  const handleTodo = (evt) => {
+    evt.preventDefault();
+
+    const newTodo={
+
+      text:evt.target[0].value,
+      isComplate:false,
+    }
+    
+    setTodos([newTodo,...todo]);
+    evt.target[0].value="";
+    
+    
+  }
+
+  let count=1;
+
+  todo.filter((e)=>{
+    e.id=count;
+    count++;
+    
+  })
+
+  window.localStorage.setItem("Todos",JSON.stringify(todo));
+
 
   return (
     <div className="container mt-5">
       <h1 className="text-center mb-4">My To do list</h1>
       <div className="inner">
         <form
-          onSubmit={(evt) => {
-            evt.preventDefault();
-            setTodos([
-              ...todo,
-              {
-                
-                name: evt.target[0].value,
-                isComplate: false,
-              },
-            ]);
-          }}
+          onSubmit={handleTodo}
         >
           <input type={"text"} className="form-control" />
         </form>
 
-        {todo.forEach((e)=>{
-          count++;
-          e.id=count;
-          console.log(e);
-        })}
+ 
 
         <ul className="list-unstyled w-75">
 
           {todo.map((e) => (
-            
-            
-            <ListItem text={e.name}  isComplate={e.isComplate} id={e.id} />
+            <ListItem key={e.id} text={e.text}  isComplate={e.isComplate} id={e.id} todo={todo} setTodos={setTodos}  />
           ))}
         </ul>
       </div>
--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -3,15 +3,37 @@ import {AiOutlineEdit} from "react-icons/ai"
 import "./ListItem.scss";
 
 
-const ListItem=({text,isComplate,id})=>{
+const ListItem=({text,isComplate,id,todo,setTodos})=>{
 
+  const handleDeleteTodo= (todoId)=>{
+    const findedTodos=todo.filter(e=>e.id!==todoId)
+
+    setTodos([...findedTodos])
+  }
+
+  const handleEditTodo=(todoId)=>{
+    todo.forEach(e => {
+      e.id==todoId?e.text=prompt("Todoni o'zgartiring:"):e.text=e.text;
+      
+    });
+
+    setTodos([...todo])
+  }
+
+  const handleCheckTodo=(todoId)=>{
+    const findedTodo=todo.find((e)=>e.id===todoId)
+
+    findedTodo.isComplate=!findedTodo.isComplate;
+    
+    setTodos([...todo])
+  }
   
   return(
     <li className="d-flex align-items-center mb-3">
-      {isComplate?<input defaultChecked="true" className="form-check-input" type={"checkbox"} />:<input className="form-check-input" type={"checkbox"} />}
-      <h1 className="m-0 ms-2 me-2 fs-4">{text}</h1>
-      <button className="edit-btn btn btn-outline-primary me-2 fs-4 " ><AiOutlineEdit /> Edit</button>
-      <button className="delete-btn btn btn-outline-danger fs-4"><AiOutlineDelete />Delete</button>
+      <input onChange={()=>handleCheckTodo(id)} defaultChecked={isComplate} className="form-check-input" type={"checkbox"}/>
+      <strong className="m-0 ms-2 me-2 fs-4 " ><span className={isComplate && "text-decoration-line-through"}>{id}  {text}</span></strong>
+      <button onClick={()=>handleEditTodo(id)} className="edit-btn btn btn-outline-primary me-2 fs-4 " ><AiOutlineEdit /> Edit</button>
+      <button onClick={()=>handleDeleteTodo(id)} className="delete-btn btn btn-outline-danger fs-4"><AiOutlineDelete />Delete</button>
     </li>
   )
 }
