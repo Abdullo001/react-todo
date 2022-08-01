4055dd963f1bcca16af36166b04011a9bb57ffa0
Abdullo <abdullofayzullayev68@gmail.com>
2022-08-01 07:36:20 +0300

    add fixxed bugs

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -13,7 +13,7 @@ const ListItem=({text,isComplate,id,todo,setTodos})=>{
 
   const handleEditTodo=(todoId)=>{
     todo.forEach(e => {
-      e.id==todoId?e.text=prompt("Todoni o'zgartiring:"):e.text=e.text;
+      e.id===todoId?e.text=prompt("Todoni o'zgartiring:"):checked="";
       
     });
 
@@ -27,11 +27,16 @@ const ListItem=({text,isComplate,id,todo,setTodos})=>{
     
     setTodos([...todo])
   }
+
+  let checked=""
+  if(isComplate){
+    checked="text-decoration-line-through"
+  }
   
   return(
     <li className="d-flex align-items-center mb-3">
       <input onChange={()=>handleCheckTodo(id)} defaultChecked={isComplate} className="form-check-input" type={"checkbox"}/>
-      <strong className="m-0 ms-2 me-2 fs-4 " ><span className={isComplate && "text-decoration-line-through"}>{id}  {text}</span></strong>
+      <strong className="m-0 ms-2 me-2 fs-4 " ><span className={checked} >{id}  {text}</span></strong>
       <button onClick={()=>handleEditTodo(id)} className="edit-btn btn btn-outline-primary me-2 fs-4 " ><AiOutlineEdit /> Edit</button>
       <button onClick={()=>handleDeleteTodo(id)} className="delete-btn btn btn-outline-danger fs-4"><AiOutlineDelete />Delete</button>
     </li>
